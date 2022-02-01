b67df7710b64e859c978277c7a118700841594da
烽正 楊 <iefcyang@gmail.com>
2022-02-01 10:48:26 +0800

    A006

--- GitTest001/Form1.Designer.cs
+++ GitTest001/Form1.Designer.cs
@@ -34,6 +34,7 @@ namespace GitTest001
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -81,11 +82,21 @@ namespace GitTest001
             this.button5.Text = "A005";
             this.button5.UseVisualStyleBackColor = true;
             // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(350, 195);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 41);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "A006";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 431);
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -104,6 +115,7 @@ namespace GitTest001
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
