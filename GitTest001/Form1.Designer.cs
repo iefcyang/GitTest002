9ce5bcc3ed3cc966c73d7a751e7a7eda53132174
烽正 楊 <iefcyang@gmail.com>
2022-02-01 09:49:02 +0800

    A004

--- GitTest001/Form1.Designer.cs
+++ GitTest001/Form1.Designer.cs
@@ -32,6 +32,7 @@ namespace GitTest001
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -54,18 +55,28 @@ namespace GitTest001
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(350, 195);
+            this.button3.Location = new System.Drawing.Point(342, 32);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(100, 41);
             this.button3.TabIndex = 2;
             this.button3.Text = "A003";
             this.button3.UseVisualStyleBackColor = true;
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(350, 195);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 41);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "A004";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 431);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -80,6 +91,7 @@ namespace GitTest001
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
